2bc7c7a7dd1e474957d0a639663afc3c81278798
Yuan Ji <jiwhiz@gmail.com>
2014-03-13 19:13:18 -0600

    change to HATEOAS

--- src/main/java/com/jiwhiz/example/config/WebConfig.java
+++ src/main/java/com/jiwhiz/example/config/WebConfig.java
@@ -1,57 +1,25 @@
 package com.jiwhiz.example.config;
 
-import java.util.Properties;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 @Configuration
 @EnableWebMvc
+@EnableHypermediaSupport(type = { HypermediaType.HAL })
 @ComponentScan(basePackages = {
         "com.jiwhiz.example.rest"
 })
 public class WebConfig extends WebMvcConfigurerAdapter {
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-    
-    @Bean
-    public SimpleMappingExceptionResolver exceptionResolver() {
-        SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
-
-        Properties exceptionMappings = new Properties();
-
-        exceptionMappings.put("java.lang.Exception", "error/error");
-        exceptionMappings.put("java.lang.RuntimeException", "error/error");
-
-        exceptionResolver.setExceptionMappings(exceptionMappings);
-
-        Properties statusCodes = new Properties();
-
-        statusCodes.put("error/404", "404");
-        statusCodes.put("error/error", "500");
-
-        exceptionResolver.setStatusCodes(statusCodes);
-
-        return exceptionResolver;
-    }
-    
+        
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer c) {
-        c.defaultContentType(MediaType.APPLICATION_JSON);
+        c.defaultContentType(MediaTypes.HAL_JSON);
     }
 
 }
--- src/main/java/com/jiwhiz/example/rest/UserRestController.java
+++ src/main/java/com/jiwhiz/example/rest/UserRestController.java
@@ -1,10 +1,13 @@
 package com.jiwhiz.example.rest;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +19,13 @@ import com.jiwhiz.example.domain.UserProfile;
 public class UserRestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
     
-    @RequestMapping(method = RequestMethod.GET, value = "/api/user", produces = MediaType.APPLICATION_JSON_VALUE)
-    public HttpEntity<UserProfile> getCurrentUserProfile() {
+    @RequestMapping(method = RequestMethod.GET, value = "/api/user", produces = "application/hal+json")
+    public HttpEntity<Resource<UserProfile>> getCurrentUserProfile() {
         LOGGER.debug("==>UserRestController.getCurrentUserProfie()");
         UserProfile profile = new UserProfile("Yuan", "yuan@jiwhiz.com");
-        return new ResponseEntity<UserProfile>(profile, HttpStatus.OK);
+        Resource<UserProfile> resource = new Resource<UserProfile>(profile);
+        resource.add(linkTo(methodOn(UserRestController.class).getCurrentUserProfile()).withSelfRel());
+        return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 
 }
